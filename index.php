8889115d5dbb264a01b6d8fea8ba72afc5d01981
Tautvilas Mečinskas <connect@tautvilas.lt>
2023-08-11 17:34:17 +0300

    use leaf fetch

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/vendor/autoload.php';
 use Leaf\Blade;
+use Leaf\Fetch;
+//use function Leaf\fetch;
 
 $blade = new Blade('views', 'views/cache');
 
@@ -14,7 +16,19 @@ app()->get('/', function () {
 
 app()->get('/posts', function () {
 	global $blade;
+
 	$posts = db()->select('posts')->all();
+	foreach ($posts as $post) {
+		$url = "http://" . $post["path"];
+		$res = Fetch::request([
+			"method" => "GET",
+			"url" => "http://" . $post["path"],
+			"rawResponse" => true,
+		]);
+		// $res = fetch("http://" + $post["domain"] + "/" + $post["path"]);
+		var_dump($res);
+		die();
+	}
 	echo $blade->make('posts', ['posts' => $posts])->render();
 });
 
